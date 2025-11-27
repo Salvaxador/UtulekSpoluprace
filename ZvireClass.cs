aa42b82f1a3556b3da42a6ccf6c069d2b1fee960
Salvaxador <129551279+Salvaxador@users.noreply.github.com>
2025-11-27 17:57:37 +0100

    Pridani Tridy zvíre
    Obsahuje atributy

---
+++ ZvireClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UtulekSpoluprace
+{
+    public class Zvire
+    {
+        public string Name { get; set; }
+        public bool Adopted { get; set; }
+        public int Age { get; set; }
+        public string AnimalType { get; set; }
+    }
+}
